15aa860794357973939380a7387fff218d2cbcf3
Kamil Kaminski <kamil.kaminski3397@gmail.com>
2023-07-25 05:38:31 +0200

    play/pause btns functionality added

--- script.js
+++ script.js
@@ -7,5 +7,26 @@ menu.addEventListener("click", () => {
   navabr.classList.toggle("change");
   menu.classList.toggle("change");
 });
+// End of Navbar
 
-// End ofNavbar
+// Section 2 Video
+
+const video = document.querySelector('.video')
+const btn = document.querySelector('.buttons i')
+const bar = document.querySelector('.video-bar')
+
+const playPause = () => {
+  if(video.paused) {
+    video.play()
+    btn.className = 'far fa-pause-circle'
+  } else {
+    video.pause()
+    btn.className = 'far fa-play-circle'
+  }
+}
+
+btn.addEventListener('click', () => {
+  playPause()
+})
+
+// End of Section 2 Video
